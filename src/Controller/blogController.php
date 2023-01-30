eefbe1e2fc0545e9a39b1c0cfb276d01f479a122
DavidChatton <m.davidchatton@hotmail.fr>
2023-01-30 16:31:16 +0100

    Création du fichier blogController.php - blog.html.twig et Modification du number.html.twig

---
+++ src/Controller/blogController.php
@@ -0,0 +1,17 @@
+<?php
+
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'blog_list')]
+    public function list(): Response
+    {
+        return $this->render('blog/blog.html.twig' []);
+    }
+}
